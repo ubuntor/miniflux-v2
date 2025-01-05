73c9f51317b3d2bfb39b7770e23739f70199238d
ubuntor <2906473+ubuntor@users.noreply.github.com>
2025-01-05 18:52:05 -0500

    use same syntax as global rules for feed rules

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -7,7 +7,10 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
+	"slices"
 	"strings"
+
+	"miniflux.app/v2/internal/locale"
 )
 
 var domainRegex = regexp.MustCompile(`^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$`)
@@ -66,4 +69,36 @@ func IsValidDomainList(value string) bool {
 	}
 
 	return true
+}
+
+func isValidFilterRules(filterEntryRules string, filterType string) *locale.LocalizedError {
+	// Valid Format: FieldName=RegEx\nFieldName=RegEx...
+	fieldNames := []string{"EntryTitle", "EntryURL", "EntryCommentsURL", "EntryContent", "EntryAuthor", "EntryTag", "EntryDate"}
+
+	rules := strings.Split(filterEntryRules, "\n")
+	for i, rule := range rules {
+		// Check if rule starts with a valid fieldName
+		idx := slices.IndexFunc(fieldNames, func(fieldName string) bool { return strings.HasPrefix(rule, fieldName) })
+		if idx == -1 {
+			return locale.NewLocalizedError("error.settings_"+filterType+"_rule_fieldname_invalid", i+1, "'"+strings.Join(fieldNames, "', '")+"'")
+		}
+		fieldName := fieldNames[idx]
+		fieldRegEx, _ := strings.CutPrefix(rule, fieldName)
+
+		// Check if regex begins with a =
+		if !strings.HasPrefix(fieldRegEx, "=") {
+			return locale.NewLocalizedError("error.settings_"+filterType+"_rule_separator_required", i+1)
+		}
+		fieldRegEx = strings.TrimPrefix(fieldRegEx, "=")
+
+		if fieldRegEx == "" {
+			return locale.NewLocalizedError("error.settings_"+filterType+"_rule_regex_required", i+1)
+		}
+
+		// Check if provided pattern is a valid RegEx
+		if !IsValidRegex(fieldRegEx) {
+			return locale.NewLocalizedError("error.settings_"+filterType+"_rule_invalid_regex", i+1)
+		}
+	}
+	return nil
 }
